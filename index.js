156c82eec40df9fabc68ddb349d9c76a985ffad6
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2023-06-13 14:04:50 -0300

    :sparkles: feat: Error handling

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { GraphQLError } = require('graphql')
 const { v1: uuid } = require('uuid')
 
 let persons = [
@@ -69,8 +70,17 @@ const resolvers = {
   },
   Mutation: {
     addPerson: (root, args) => {
-      console.log('🔴 root', root)
-      console.log('🔴 args', args)
+      // root is null
+      // args is the object passed to the mutation
+
+      if (persons.find((p) => p.name === args.name)) {
+        throw new GraphQLError('Name must be unique', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: [args.name],
+          },
+        })
+      }
 
       const person = { ...args, id: uuid() }
       persons = persons.concat(person)
