49f996c3627914d34cf3667a020532969201d542
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2023-06-13 14:31:38 -0300

    :sparkles: feat: Enum

--- index.js
+++ index.js
@@ -3,27 +3,7 @@ const { startStandaloneServer } = require('@apollo/server/standalone')
 const { GraphQLError } = require('graphql')
 const { v1: uuid } = require('uuid')
 
-let persons = [
-  {
-    name: 'Arto Hellas',
-    phone: '040-123543',
-    street: 'Tapiolankatu 5 A',
-    city: 'Espoo',
-    id: '3d594650-3436-11e9-bc57-8b80ba54c431',
-  },
-  {
-    name: 'Matti Luukkainen',
-    phone: '040-432342',
-    Address: { street: 'Malminkaari 10 A', city: 'Helsinki' },
-    id: '3d599470-3436-11e9-bc57-8b80ba54c431',
-  },
-  {
-    name: 'Venla Ruuska',
-    street: 'Nallemäentie 22 C',
-    city: 'Helsinki',
-    id: '3d599471-3436-11e9-bc57-8b80ba54c431',
-  },
-]
+const persons = require('./persons')
 
 const typeDefs = `
   type Address {
@@ -31,16 +11,22 @@ const typeDefs = `
     city: String!
   }
 
+  enum YesNo {
+    YES
+    NO
+  }
+
   type Person {
     name: String!
     phone: String
     address: Address!
     id: ID!
+    nationality: String!
   }
 
   type Query {
     personCount: Int!
-    allPersons: [Person!]!
+    allPersons(phone: YesNo): [Person!]!
     findPerson(name: String!): Person
   }
 
@@ -50,6 +36,7 @@ const typeDefs = `
       phone: String
       street: String!
       city: String!
+      nationality: String!
     ): Person
   }
 `
@@ -57,7 +44,16 @@ const typeDefs = `
 const resolvers = {
   Query: {
     personCount: () => persons.length,
-    allPersons: () => persons,
+    allPersons: (root, args) => {
+      if (!args.phone) {
+        return persons
+      }
+
+      const byPhone = (person) =>
+        args.phone === 'YES' ? person.phone : !person.phone
+
+      return persons.filter(byPhone)
+    },
     findPerson: (root, args) => persons.find((p) => p.name === args.name),
   },
   Person: {
