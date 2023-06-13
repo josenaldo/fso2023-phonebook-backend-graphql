1571a2864cffef2283406e2aeb43d82323eb1952
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2023-06-13 15:01:05 -0300

    :sparkles: feat: Changing a phone number

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { startStandaloneServer } = require('@apollo/server/standalone')
 const { GraphQLError } = require('graphql')
 const { v1: uuid } = require('uuid')
 
-const persons = require('./persons')
+let persons = require('./persons')
 
 const typeDefs = `
   type Address {
@@ -37,6 +37,10 @@ const typeDefs = `
       street: String!
       city: String!
       nationality: String!
+    ): Person
+    editNumber(
+      name: String!
+      phone: String!
     ): Person
   }
 `
@@ -81,6 +85,16 @@ const resolvers = {
       const person = { ...args, id: uuid() }
       persons = persons.concat(person)
       return person
+    },
+    editNumber: (root, args) => {
+      const person = persons.find((p) => p.name === args.name)
+      if (!person) {
+        return null
+      }
+
+      const updatedPerson = { ...person, phone: args.phone }
+      persons = persons.map((p) => (p.name === args.name ? updatedPerson : p))
+      return updatedPerson
     },
   },
 }
